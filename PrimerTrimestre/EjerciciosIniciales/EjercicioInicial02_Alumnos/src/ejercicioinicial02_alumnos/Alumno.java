3c59176e8346a127521bbca1f6fd239e31841856
Haoen219 <116910268+Haoen219@users.noreply.github.com>
2023-09-14 19:48:39 +0200

    Add files via upload

--- PrimerTrimestre/EjerciciosIniciales/EjercicioInicial02_Alumnos/src/ejercicioinicial02_alumnos/Alumno.java
+++ PrimerTrimestre/EjerciciosIniciales/EjercicioInicial02_Alumnos/src/ejercicioinicial02_alumnos/Alumno.java
@@ -10,10 +10,12 @@ import java.util.ArrayList;
  *
  * @author 2DAM_Zhang_Haoen
  */
-public class Alumno {
+public class Alumno implements Comparable<Alumno> {
     private String nombre;
     private ArrayList<Double> notas= new ArrayList<>();
     private int aprobados;
+    private float porcentajeAprobado;
+    
     
     public Alumno (String nombre, double... notas){
         this.nombre=nombre;
@@ -23,5 +25,39 @@ public class Alumno {
                 this.aprobados++;
             }
         }
+        this.porcentajeAprobado=(this.aprobados*100)/this.notas.size();
+        System.out.println(this.porcentajeAprobado);
+    }
+    
+    @Override
+    public int compareTo(Alumno o) {
+        int resultado=0;
+        if (this.getPorcentajeAprobado()<o.getPorcentajeAprobado()) {
+            resultado=-1;
+        }else if (this.getPorcentajeAprobado()>o.getPorcentajeAprobado()) {
+            resultado=1; 
+        }else resultado=0;
+        
+        if(resultado==0){
+            if (this.getAprobados()<o.getAprobados()) {
+                resultado=-1; 
+            }else if (this.getAprobados()>o.getAprobados()) {
+                resultado=1;
+            }else resultado=0;
+        }
+        return resultado;
+    }
+
+    public ArrayList<Double> getNotas() {
+        return notas;
+    }
+    public int getAprobados() {
+        return aprobados;
+    }
+    public double getPorcentajeAprobado() {
+        return porcentajeAprobado;
+    }
+    public String getNombre() {
+        return nombre;
     }
 }
