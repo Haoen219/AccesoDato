ac146a54f36459a71b01fd039ae12122c20c473b
Haoen219 <116910268+Haoen219@users.noreply.github.com>
2024-02-08 12:08:51 +0100

    funciones creadas

--- SegundoTrimestre_AD/Practica06_eXist/src/main/java/utilidades/CRUD_EXIST.java
+++ SegundoTrimestre_AD/Practica06_eXist/src/main/java/utilidades/CRUD_EXIST.java
@@ -1,15 +1,8 @@
 package utilidades;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.exist.collections.Collection;
 import org.w3c.dom.Document;
-import org.exist.xmldb.EXistResource;
-import org.xmldb.api.base.Resource;
 import org.xmldb.api.base.ResourceIterator;
 import org.xmldb.api.base.ResourceSet;
 import org.xmldb.api.base.XMLDBException;
@@ -437,94 +430,213 @@ public class CRUD_EXIST {
                 System.out.println("Recurso XML no encontrado.");
             }
         } catch (XMLDBException e) {
-            System.out.println("Error insertando Alumno");
+            System.out.println("Error actualizando Alumno");
             e.printStackTrace();
         }
         return false;
     }
 
     public static boolean actualizarModulo(String id, String nombre) {
+        String xpathQuery = "/" + modulo_tabla;
         try {
-            Gestor.getConnection().createStatement()
-                    .execute("UPDATE modulo SET modulo_nombre = " + nombre + " WHERE modulo_id = '"
-                            + id + "'");
-        } catch (SQLException ex) {
-            System.out.println("Error actualizando modulon" + ex);
+            XMLResource resource = (XMLResource) Gestor.getExistCollection().getResource(xpathQuery);
+            if (resource != null) {
+                Document doc = (Document) resource.getContentAsDOM();
+
+                NodeList modulos = doc.getElementsByTagName(modulo_tabla);
+                Element modulo;
+
+                for (int i = 0; i < modulos.getLength(); i++) {
+                    modulo = (Element) modulos.item(i);
+                    Element idElement = (Element) modulo.getElementsByTagName(modulo_id).item(0);
+
+                    if (idElement.getTextContent().equals(id)) {
+                        Element nombreElement = (Element) modulo.getElementsByTagName(modulo_nombre).item(0);
+                        nombreElement.setTextContent(nombre);
+
+                        // Actualizar el recurso en la colección
+                        Gestor.getExistCollection().storeResource(resource);
+                        return true;
+                    }
+                }
+            } else {
+                System.out.println("Recurso XML no encontrado.");
+            }
+        } catch (XMLDBException e) {
+            System.out.println("Error actualizando Modulo");
+            e.printStackTrace();
         }
         return false;
     }
 
     public static boolean actualizarNota(String id, int nota1, int nota2, int nota3) {
+        String xpathQuery = "/" + notas_tabla;
         try {
-            Gestor.getConnection().createStatement()
-                    .execute("UPDATE notas SET nota1 = " + nota1 + ", nota2 = " + nota2 + ", nota3 =" + nota3
-                            + " WHERE notas_id = '"
-                            + id + "'");
-        } catch (SQLException ex) {
-            System.out.println("Error actualizando notasn" + ex);
+            XMLResource resource = (XMLResource) Gestor.getExistCollection().getResource(xpathQuery);
+            if (resource != null) {
+                Document doc = (Document) resource.getContentAsDOM();
+
+                NodeList notas = doc.getElementsByTagName(notas_tabla);
+                Element nota;
+
+                for (int i = 0; i < notas.getLength(); i++) {
+                    nota = (Element) notas.item(i);
+                    Element idElement = (Element) nota.getElementsByTagName(notas_id).item(0);
+
+                    if (idElement.getTextContent().equals(id)) {
+                        Element nota1Element = (Element) nota.getElementsByTagName(notas_nota1).item(0);
+                        nota1Element.setTextContent(Integer.toString(nota1));
+                        Element nota2Element = (Element) nota.getElementsByTagName(notas_nota2).item(0);
+                        nota2Element.setTextContent(Integer.toString(nota2));
+                        Element nota3Element = (Element) nota.getElementsByTagName(notas_nota3).item(0);
+                        nota3Element.setTextContent(Integer.toString(nota3));
+
+                        // Actualizar el recurso en la colección
+                        Gestor.getExistCollection().storeResource(resource);
+                        return true;
+                    }
+                }
+            } else {
+                System.out.println("Recurso XML no encontrado.");
+            }
+        } catch (XMLDBException e) {
+            System.out.println("Error actualizando Notas");
+            e.printStackTrace();
         }
         return false;
     }
 
     public static boolean actualizarMatricula(String id, String calificacion) {
+        String xpathQuery = "/" + notas_tabla;
         try {
-            Gestor.getConnection().createStatement().execute(
-                    "UPDATE matricula SET calificacion = '" + calificacion + "' WHERE matricula_id = '" + id + "'");
-        } catch (SQLException ex) {
-            System.out.println("Error actualizando matriculan" + ex);
+            XMLResource resource = (XMLResource) Gestor.getExistCollection().getResource(xpathQuery);
+            if (resource != null) {
+                Document doc = (Document) resource.getContentAsDOM();
+
+                NodeList matriculas = doc.getElementsByTagName(matricula_tabla);
+                Element matricula;
+
+                for (int i = 0; i < matriculas.getLength(); i++) {
+                    matricula = (Element) matriculas.item(i);
+                    Element idElement = (Element) matricula.getElementsByTagName(matricula_id).item(0);
+
+                    if (idElement.getTextContent().equals(id)) {
+                        Element calificacioElement = (Element) matricula.getElementsByTagName(matricula_calificacion)
+                                .item(0);
+                        calificacioElement.setTextContent(calificacion);
+
+                        // Actualizar el recurso en la colección
+                        Gestor.getExistCollection().storeResource(resource);
+                        return true;
+                    }
+                }
+            } else {
+                System.out.println("Recurso XML no encontrado.");
+            }
+        } catch (XMLDBException e) {
+            System.out.println("Error actualizando Matricula ()");
+            e.printStackTrace();
         }
         return false;
     }
 
-    public static boolean actualizarMatricula(String id, String alu_id, String modu_id, String nota_id,
-            String calificacion) {
+    public static boolean actualizarMatricula(String id, String alu_id, String modu_id, String nota_id, String calificacion) {
+        String xpathQuery = "/" + notas_tabla;
         try {
-            Gestor.getConnection().createStatement().execute(
-                    "UPDATE matricula SET calificacion = '" + calificacion + "', alumno_id = '" + alu_id
-                            + "', modulo_id = '" + modu_id + "', notas_id = '" + notas_id + "'  WHERE matricula_id = '"
-                            + id + "'");
-        } catch (SQLException ex) {
-            System.out.println("Error actualizando matriculan" + ex);
+            XMLResource resource = (XMLResource) Gestor.getExistCollection().getResource(xpathQuery);
+            if (resource != null) {
+                Document doc = (Document) resource.getContentAsDOM();
+
+                NodeList matriculas = doc.getElementsByTagName(matricula_tabla);
+                Element matricula;
+
+                for (int i = 0; i < matriculas.getLength(); i++) {
+                    matricula = (Element) matriculas.item(i);
+                    Element idElement = (Element) matricula.getElementsByTagName(matricula_id).item(0);
+
+                    if (idElement.getTextContent().equals(id)) {
+                        Element alumnoElement = (Element) matricula.getElementsByTagName(matricula_alumno_id).item(0);
+                        alumnoElement.setTextContent(alu_id);
+                        Element moduloElement = (Element) matricula.getElementsByTagName(matricula_modulo_id).item(0);
+                        moduloElement.setTextContent(modu_id);
+                        Element notasElement = (Element) matricula.getElementsByTagName(matricula_notas_id).item(0);
+                        notasElement.setTextContent(nota_id);
+                        Element calificacioElement = (Element) matricula.getElementsByTagName(matricula_calificacion)
+                                .item(0);
+                        calificacioElement.setTextContent(calificacion);
+
+                        // Actualizar el recurso en la colección
+                        Gestor.getExistCollection().storeResource(resource);
+                        return true;
+                    }
+                }
+            } else {
+                System.out.println("Recurso XML no encontrado.");
+            }
+        } catch (XMLDBException e) {
+            System.out.println("Error actualizando Matricula");
+            e.printStackTrace();
         }
         return false;
     }
 
     // DELETE
     public static boolean borrarAlumno(String id) {
+        String xquery = "update delete collection('" + alumno_tabla + "')//" + alumno_tabla + "[" + alumno_id + "='" + id + "']";
+
         try {
-            return Gestor.getConnection().createStatement()
-                    .execute("DELETE FROM alumno WHERE alumno_nia = '" + id + "'");
-        } catch (SQLException ex) {
-            System.out.println("Error borrando alumno NIA:" + id + "n" + ex);
+            XQueryService queryService = (XQueryService) Gestor.getExistCollection().getService("XQueryService", "1.0");
+            queryService.query(xquery);
+            System.out.println("El alumno con ID " + id + " ha sido borrado exitosamente");
+            return true;
+        } catch (XMLDBException e) {
+            System.out.println("Error borrando Alumno");
+            e.printStackTrace();
         }
         return false;
     }
 
     public static boolean borrarModulo(String id) {
+        String xquery = "update delete collection('" + modulo_tabla + "')//" + modulo_tabla + "[" + modulo_id + "='" + id + "']";
+
         try {
-            return Gestor.getConnection().createStatement()
-                    .execute("DELETE FROM modulo WHERE modulo_id = '" + id + "'");
-        } catch (SQLException ex) {
-            System.out.println("Error borrando modulo ID:" + id + "n" + ex);
+            XQueryService queryService = (XQueryService) Gestor.getExistCollection().getService("XQueryService", "1.0");
+            queryService.query(xquery);
+            System.out.println("El modulo con ID " + id + " ha sido borrado exitosamente");
+            return true;
+        } catch (XMLDBException e) {
+            System.out.println("Error borrando Modulo");
+            e.printStackTrace();
         }
         return false;
     }
 
     public static boolean borrarNotas(String id) {
+        String xquery = "update delete collection('" + notas_tabla + "')//" + notas_tabla + "[" + notas_id + "='" + id + "']";
+
         try {
-            return Gestor.getConnection().createStatement().execute("DELETE FROM notas WHERE notas_id = '" + id + "'");
-        } catch (SQLException ex) {
-            System.out.println("Error borrando notas ID:" + id + "n" + ex);
+            XQueryService queryService = (XQueryService) Gestor.getExistCollection().getService("XQueryService", "1.0");
+            queryService.query(xquery);
+            System.out.println("Las notas con ID " + id + " ha sido borrado exitosamente");
+            return true;
+        } catch (XMLDBException e) {
+            System.out.println("Error borrando Notas");
+            e.printStackTrace();
         }
         return false;
     }
 
     public static boolean borrarMatricula(String id) {
+        String xquery = "update delete collection('" + matricula_tabla + "')//" + matricula_tabla + "[" + matricula_id + "='" + id + "']";
+
         try {
-            return Gestor.getConnection().createStatement()
-                    .execute("DELETE FROM matricula WHERE matricula_id = '" + id + "'");
-        } catch (SQLException ex) {
-            System.out.println("Error borrando matricula ID:" + id + "n" + ex);
+            XQueryService queryService = (XQueryService) Gestor.getExistCollection().getService("XQueryService", "1.0");
+            queryService.query(xquery);
+            System.out.println("La matricula con ID " + id + " ha sido borrado exitosamente");
+            return true;
+        } catch (XMLDBException e) {
+            System.out.println("Error borrando Matricula");
+            e.printStackTrace();
         }
         return false;
     }
